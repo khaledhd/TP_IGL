35709b38a67057b4643f9f1ef38bd9781572421b
khaledhd <yahiakiki@gmail.com>
2017-10-20 00:11:37 +0100

    001

--- src/IGL/VecteurHelper.java
+++ src/IGL/VecteurHelper.java
@@ -5,4 +5,55 @@ package IGL;
  */
 public class VecteurHelper {
 
+
+
+    /**
+     * Extraire le Min et le Max d'un Vecteur
+     * @param tab  Vecteur a traiter
+     * @return Un tableau de deux cases contenant le Min et le MAx
+     */
+    public  int[] minMax(int[] tab) {
+        int minn = tab[0];
+        int maxx = tab[0];
+        for (int i = 1; i < tab.length; i++) {
+            if (tab[i] < minn) {
+                minn = tab[i];
+            }
+            if (tab[i] > maxx) {
+                maxx = tab[i];
+            }
+
+        }
+        int t[]=new int[2] ;
+        t[0]=minn;
+        t[1]=maxx;
+        return  t;
+
+    }
+
+    /**
+     * Effectuer l'addition de deux Vecteurs
+     * @param tab1 Premier Vecteur
+     * @param tab2 Deuxieme Vecteur
+     * @return  Un tableau contenant le resultat de la somme
+     */
+    public  int[] addArr(int[] tab1,int[] tab2) {
+
+      if(tab1.length!=tab2.length) {
+      return null;
+      }
+      else
+      {
+          int resultat[]=new int [tab1.length];
+          for(int i=0;i<tab1.length;i++)
+          {
+              resultat[i]=tab1[i]+tab2[i];
+
+          }
+          return resultat;
+      }
+    }
+
+
+
 }
